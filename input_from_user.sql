1be5657fd152cab3f36325b1b726f4c4cc85dc9c
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-06-18 17:53:49 +0530

    Add SQL script for user input declaration

---
+++ input_from_user.sql
@@ -0,0 +1,15 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+
+    -- taking input for variable a
+    a NUMBER := &a;
+
+    -- taking input for variable b
+    b VARCHAR2(30) := &b;
+
+BEGIN
+    NULL;
+
+END;
+/
